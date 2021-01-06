d888858f244102432d46df38d3e2f50967c8cd7e
subhajit104 <subhajit.manna.104@gmail.com>
2021-01-07 01:04:35 +0530

    add taiwind css

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const path = require('path')
 const ejs = require('ejs')
 const expressLayouts = require('express-ejs-layouts')
 
-// express app 
+// Express app 
 const app = express()
 
-// sertting the port number. 
+// Sertting the port number.
 const PORT = process.env.PORT || 3000
+
 app.use(expressLayouts)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
